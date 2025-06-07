d31ef6ab26e55dccaf03a6f77d3ad55bdefc6443
Ian Gibbs <ian@attono.net>
2025-06-07 21:17:04 +0100

    Working ncurses version

--- message_handler.pl
+++ message_handler.pl
@@ -1,29 +1,20 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
-use Redis;
 use Log::Log4perl qw(:easy);
-use FindBin qw($Bin);
-use YAML::Tiny;
+use YAML::Tiny 'LoadFile';
+use Redis;
 use JSON::MaybeXS;
 
+# --- Setup ---
 Log::Log4perl->easy_init($INFO);
-my $log = Log::Log4perl->get_logger('BMW::CSC::MessageHandler');
+my $log = get_logger();
 
-# --- Load CSC ID Configuration from YAML ---
-my $config_file = "$Bin/webapp/config.yml"; 
-my $config;
-
-eval {
-    my $yaml_docs = YAML::Tiny->read($config_file);
-    # The parsed data is the first document in the file.
-    $config = $yaml_docs->[0];
-};
-if ($@ || !defined($config)) {
-    $log->logdie("FATAL: Could not read or parse YAML config file '$config_file': " . ($@ || 'File is empty'));
-}
-
-$log->info("Successfully loaded CSC ID configuration from $config_file");
+# UPDATED: Path to the configuration file.
+my $config_file = './webapp/config.yml';
+my $config = LoadFile($config_file) 
+    or die "Could not load YAML from '$config_file'";
 
 # --- In-memory ID info map for fast lookups ---
 my %id_info;
@@ -31,23 +22,24 @@ foreach my $csc (keys %{ $config->{csc_ids} }) {
     foreach my $id (keys %{ $config->{csc_ids}->{$csc} }) {
         my $id_data = $config->{csc_ids}->{$csc}->{$id};
         $id_info{$id} = {
-            csc      => $csc,
-            type     => $id_data->{type},
-            # Store the cell_map if it exists for this ID
-            cell_map => $id_data->{cell_map} || undef,
+            csc        => $csc,
+            type       => $id_data->{type},
+            cell_map   => $id_data->{cell_map}   || undef,
+            sensor_map => $id_data->{sensor_map} || undef,
         };
     }
 }
 $log->info("Created reverse lookup map for " . scalar(keys %id_info) . " CAN IDs.");
 
-# Connect to the Redis server (assumes default localhost:6379)
-my $redis = Redis->new or $log->logdie("Cannot connect to Redis server");
+# Connect to the Redis server
+my $redis = Redis->new or die "Cannot connect to Redis server";
 $log->info("Connected to Redis.");
 
 my $json_coder = JSON::MaybeXS->new(utf8 => 1);
 my $message_count = 0;
-my $child_script_cmd = "perl $FindBin::Bin/data_streamer.pl";
-open my $child_handle, '-|', $child_script_cmd or die "Can't run child script: $!";
+my $child_script_cmd = "perl ./data_streamer.pl"; # Assumes data_streamer is in the same directory
+open my $child_handle, '-|', $child_script_cmd 
+    or die "Can't run child script '$child_script_cmd': $!";
 
 # --- Main Loop ---
 while (my $line = <$child_handle>) {
@@ -61,6 +53,8 @@ while (my $line = <$child_handle>) {
             my ($csc, $type) = ($info->{csc}, $info->{type});
             my $update_payload;
 
+            $redis->set('bms:heartbeat', time());
+
             if ($type eq 'voltage' && $info->{cell_map}) {
                 my @bytes = split /\s+/, $data;
                 my %voltages;
@@ -69,58 +63,51 @@ while (my $line = <$child_handle>) {
                     if (defined $bytes[$start_byte] && defined $bytes[$start_byte + 1]) {
                         my $voltage = sprintf("%.2f", convert_bytes_to_voltage($bytes[$start_byte], $bytes[$start_byte + 1]));
                         $voltages{$cell_num} = $voltage;
-                        # Set the voltage for the specific cell in the CSC's hash
                         $redis->hset("bms:csc:$csc", $cell_num, $voltage);
                     }
                 }
                 $update_payload = { csc => $csc, id => $id, type => 'voltage', voltages => \%voltages };
+                $redis->publish('bms:updates', $json_coder->encode($update_payload));
+                $log->trace("[Msg $message_count] Processed voltages for CSC $csc, ID $id");
+            
+            } elsif ($type eq 'temperature' && $info->{sensor_map}) {
+                my @bytes = split /\s+/, $data;
+                my %sensor_temps;
+                foreach my $byte_index (keys %{$info->{sensor_map}}) {
+                    my $sensor_num = $info->{sensor_map}->{$byte_index};
+                     if (defined $bytes[$byte_index]) {
+                        my $temp = convert_byte_to_temp($bytes[$byte_index]);
+                        $sensor_temps{$sensor_num} = $temp;
+                        $redis->hset("bms:csc_temps:$csc", $sensor_num, $temp);
+                    }
+                }
+                $update_payload = { csc => $csc, id => $id, type => 'temperature', temps => \%sensor_temps };
+                $redis->publish('bms:updates', $json_coder->encode($update_payload));
+                $log->trace("[Msg $message_count] Processed temperatures for CSC $csc, ID $id");
+
             } else {
-                # For unknown types, just store the raw data
                 $redis->hset("bms:csc_raw:$csc", $id, $data);
                 $update_payload = { csc => $csc, id => $id, type => 'unknown', data => $data };
+                $redis->publish('bms:updates', $json_coder->encode($update_payload));
+                $log->trace("[Msg $message_count] Stored raw data for CSC $csc, ID $id, Type $type");
             }
-            
-            # Publish a notification of the update for the logger
-            $redis->publish('bms:updates', $json_coder->encode($update_payload));
-            
-            # Update the global heartbeat
-            $redis->set('bms:heartbeat', time());
-            
-            $log->trace("[Msg $message_count] Wrote ID $id to Redis.");
         }
     }
 }
 
-# --- Child Exit Handling ---
-# The code reaches here only after the while loop exits, which happens
-# when the child process terminates and the pipe is closed.
 close $child_handle;
+$log->info("Child process exited. Message handler stopping.");
 
-my $child_exit_status = $? >> 8;
-$log->info("Child process has exited. Exit code: $child_exit_status");
+# --- Subroutines ---
 
 sub convert_bytes_to_voltage {
     my ($byte1_hex, $byte2_hex) = @_;
-
-    # 1. Convert each byte from hex to a decimal number.
-    my $dec1 = hex($byte1_hex);
-    my $dec2 = hex($byte2_hex);
-
-    # 2. Concatenate the numbers as strings (second number first).
-    my $concatenated = $dec2 . $dec1;
-
-    # 3. Conditional: If > 9999, place a decimal point after the 4th digit.
-    if ($concatenated > 9999) {
-        substr($concatenated, 4, 0, '.');
-    }
-
-    # 4. Divide by 1000.
-    my $divided = $concatenated / 1000;
-
-    # 5. Add 2.
-    my $final_voltage = $divided + 2;
-
-    return $final_voltage;
+    my $hex_string = $byte1_hex . $byte2_hex;
+    my $decimal_value = hex($hex_string);
+    return $decimal_value / 1000.0;
 }
 
-exit 0;
+sub convert_byte_to_temp {
+    my ($byte_hex) = @_;
+    return hex($byte_hex) - 40;
+}
--- voltage_monitor.pl
+++ voltage_monitor.pl
@@ -6,113 +6,151 @@ use Curses;
 use Redis;
 use Time::HiRes qw(sleep);
 
+# Define color pair numbers for clarity
+use constant {
+    PAIR_DEFAULT    => 0,
+    PAIR_ALERT      => 1,
+    PAIR_TEMP_OK    => 2,
+};
+
 # --- Curses Initialization ---
-initscr();      # Initialize the Curses screen
-noecho();       # Don't echo user's keypresses
-curs_set(0);    # Hide the cursor
-nodelay(1);     # Make getch() non-blocking
+initscr();
+noecho();
+curs_set(0);
+nodelay(1);
 
 # --- Color and Layout Setup ---
 if (has_colors()) {
     start_color();
-    # Initialize color pair 1: White text on a Red background for alerts.
-    init_pair(1, COLOR_WHITE, COLOR_RED);
+    # Pair 1: White text on a Red background for alerts.
+    init_pair(PAIR_ALERT, COLOR_WHITE, COLOR_RED);
+    # Pair 2: White text on a Blue background for normal temperatures.
+    init_pair(PAIR_TEMP_OK, COLOR_WHITE, COLOR_BLUE);
 }
 
-# Use the LINES and COLS functions to get screen dimensions.
 my ($max_y, $max_x) = (LINES, COLS);
-
-# Increased minimum width for new layout.
-if ($max_y < 12 || $max_x < 105) {
+# Increased minimum width for new layout including temperatures.
+if ($max_y < 12 || $max_x < 125) {
     endwin();
-    die "Terminal is too small. Minimum size is 105x12.\n";
+    die "Terminal is too small. Minimum size is 125x12.\n";
 }
 
 # --- Redis Connection ---
 my $redis = Redis->new or die_gracefully("Could not connect to Redis server.");
 
-# --- Main Loop ---
+# --- Main Application Loop ---
 while (1) {
     my $key = getch();
     last if ($key eq 'q');
 
+    # --- Data Aggregation ---
     my $last_heartbeat = $redis->get('bms:heartbeat') || 0;
     my $heartbeat_age = time() - $last_heartbeat;
+    
+    my %display_voltages;
+    my %display_temps;
 
-    my %display_data;
     if ($heartbeat_age < 10) {
         for my $csc_num (1..6) {
-            # CORRECTED: Assign the flat list returned by hgetall to a hash.
-            my %voltages = $redis->hgetall("bms:csc:$csc_num");
-
-            # Now iterate over the keys of the correctly populated hash.
-            foreach my $cell_num (keys %voltages) {
-                $display_data{$csc_num}[$cell_num] = $voltages{$cell_num};
+            # Get all voltages for the CSC
+            my %v_data = $redis->hgetall("bms:csc:$csc_num");
+            foreach my $cell_num (keys %v_data) {
+                $display_voltages{$csc_num}[$cell_num] = $v_data{$cell_num};
+            }
+            # Get all temperatures for the CSC
+            my %t_data = $redis->hgetall("bms:csc_temps:$csc_num");
+            foreach my $sensor_num (keys %t_data) {
+                $display_temps{$csc_num}[$sensor_num] = $t_data{$sensor_num};
             }
         }
     }
 
-    draw_screen(\%display_data, $heartbeat_age);
+    draw_screen(\%display_voltages, \%display_temps, $heartbeat_age);
     sleep(1);
 }
+
 # --- Cleanup ---
-endwin(); # Restore terminal settings
+endwin();
 exit 0;
+
+# --- Subroutines ---
 
 #--------------------------------------------------------------------------
 # Subroutine: draw_screen
 #
-# Renders the entire terminal display with conditional highlighting.
+# Renders the entire terminal display with conditional highlighting
+# and final layout adjustments.
 #--------------------------------------------------------------------------
 sub draw_screen {
-    my ($data) = @_;
+    my ($voltages, $temps, $heartbeat_age) = @_;
     erase(); # Clear the screen
 
     # --- Header ---
     my $time_str = localtime();
-    addstr(0, 0, "BMW Cell Voltage Monitor");
+    addstr(0, 0, "BMW CSC Monitor");
     addstr(0, $max_x - length($time_str), $time_str);
-    addstr(2, 10, "  1     2     3     4     5     6     7     8     9    10    11    12    13    14    15    16");
-    addstr(3, 10, "----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----");
+    
+    # --- Stale Data Check ---
+    if ($heartbeat_age > 5) {
+        my $warning = "STALE DATA: HANDLER NOT RUNNING OR UNRESPONSIVE";
+        attron(COLOR_PAIR(PAIR_ALERT));
+        addstr(int($max_y / 2), int(($max_x - length($warning)) / 2), $warning);
+        attroff(COLOR_PAIR(PAIR_ALERT));
+        addstr($max_y - 1, 0, "Press 'q' to exit.");
+        refresh();
+        return;
+    }
+    
+    # --- Table Headers (Final Spacing) ---
+    addstr(2, 10, "  1     2     3     4     5     6     7     8     9    10    11    12    13    14    15   16   |  T1   T2   T3");
+    addstr(3, 10, "----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ---- | ---- ---- ----");
 
-    # --- Body ---
+    # --- Table Body ---
     for my $csc_num (1..6) {
         my $y_pos = $csc_num + 3;
         addstr($y_pos, 0, "CSC $csc_num:");
 
+        # Display Voltages
         for my $cell_num (1..16) {
-            # New x-position calculation for more space.
             my $x_pos = 10 + (($cell_num - 1) * 6);
-            my $voltage_val = $data->{$csc_num}[$cell_num];
-            my $display_str = defined($voltage_val) ? $voltage_val : " ... ";
-
-            # --- Conditional Highlighting Logic ---
-            my $is_implausible = 0;
-            if (defined($voltage_val) && ($voltage_val > 4.2 || $voltage_val < 3.0)) {
-                $is_implausible = 1;
+            my $v_val = $voltages->{$csc_num}[$cell_num];
+            my $v_str = defined($v_val) ? sprintf("%4.2f", $v_val) : "....";
+            
+            if (defined($v_val) && ($v_val > 4.2 || $v_val < 3.0)) {
+                attron(COLOR_PAIR(PAIR_ALERT));
             }
+            addstr($y_pos, $x_pos, $v_str);
+            attroff(COLOR_PAIR(PAIR_ALERT));
+        }
 
-            if ($is_implausible) {
-                attron(COLOR_PAIR(1));
+        # Display Temperatures (Final Spacing)
+        addstr($y_pos, 105, "|"); # UPDATED: y-offset set to 106
+        for my $sensor_num (1..3) {
+            # UPDATED: x-offset set to 108
+            my $x_pos = 108 + (($sensor_num - 1) * 5);
+            my $t_val = $temps->{$csc_num}[$sensor_num];
+            my $t_str = defined($t_val) ? sprintf("%4.1f", $t_val) : "....";
+            
+            my $color_pair = PAIR_DEFAULT;
+            if (defined($t_val)) {
+                if ($t_val < 0 || $t_val > 50) {
+                    $color_pair = PAIR_ALERT;
+                } else {
+                    $color_pair = PAIR_TEMP_OK;
+                }
             }
-
-            addstr($y_pos, $x_pos, $display_str);
-
-            if ($is_implausible) {
-                attroff(COLOR_PAIR(1));
-            }
+            
+            attron(COLOR_PAIR($color_pair));
+            addstr($y_pos, $x_pos, $t_str);
+            attroff(COLOR_PAIR($color_pair));
         }
     }
 
     # --- Footer ---
     addstr($max_y - 1, 0, "Press 'q' to exit.");
-    refresh(); # Push buffer to terminal
+    refresh();
 }
-#--------------------------------------------------------------------------
-# Subroutine: die_gracefully
-#
-# Ensures Curses is shut down before the program exits on an error.
-#--------------------------------------------------------------------------
+
 sub die_gracefully {
     my ($message) = @_;
     endwin();
